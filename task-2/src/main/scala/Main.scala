20e3d686af0f175ebe67998dde4b9dca609f353e
https://rosteelton@github.com <Rosteelton@gmail.com>
2020-05-28 09:11:58 +0300

    delete

--- task-2/src/main/scala/Main.scala
+++
@@ -1,5 +0,0 @@
-package task2
-
-object Main extends App {
-  println("Hello, task2")
-}
--- task-2/src/test/scala/task2/MultiQueueTest.scala
+++
@@ -1,9 +0,0 @@
-package task2
-
-import org.scalatest.{FreeSpec, MustMatchers}
-
-class MultiQueueTest extends FreeSpec with MustMatchers {
-  "trivial" in {
-    true mustBe true
-  }
-}
